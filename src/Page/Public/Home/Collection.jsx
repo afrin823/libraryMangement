ba7c70788615f7b28ab1f7d12011c897709989cf
rajiul93 <rajiulislam283@gmail.com>
2025-02-16 22:27:14 +0600

    chanck

--- src/Page/Public/Home/Collection.jsx
+++ src/Page/Public/Home/Collection.jsx
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
+import useAxiosPublic from "../../../hook/useAxiosPublic";
+import BooksLoader from "../../../loader/BooksLoader";
 import BooksCard from "../PublicComponent/collection/BooksCard";
 import SectionTitle from "../PublicComponent/SectionTitle";
 import ViewAllBtn from "../PublicComponent/ViewAllBtn";
-import useAxiosPublic from "../../../hook/useAxiosPublic";
-import BooksLoader from "../../../loader/BooksLoader";
 const Collection = () => {
   const { t } = useTranslation();
   const axiosPublic = useAxiosPublic();
 
-  const { isPending, data: collections } = useQuery({
+  const { isPending, data: collections=[] } = useQuery({
     queryKey: ['collection'],
     queryFn: async () => {
-      const res = await axiosPublic(`/book?fields=bookName,_id,writerName,publisher,registrationNo,quantity,bookshelfLocation,year,image,edition,ISBN,language,category,description,rating&limit=4&sort=bookName`); 
+      const res = await axiosPublic(`/book?fields=bookName,,writerName,publisher,quantity,image,rating&limit=4&sort=bookName`); 
       return res.data.data; 
     }
   });
@@ -29,7 +29,7 @@ const Collection = () => {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {collections?.slice(0,4).map((book) => (
+        {collections?.map((book) => (
           <BooksCard key={book.bookName} collection={book} />
         ))}
       </div>
